4ee90a4fc8bb43ee567997abf79722a0f41dfc6d
LDHugeman <luisdavid.vp@gmail.com>
2021-11-24 17:43:00 +0100

    Clase 8

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor("Anahí Salgado");
+        Doctor myDoctor = new Doctor();
         myDoctor.name = "Alejandro Rodríguez";
         myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.showId();
+
+        showMenu();
+
     }
 }
