67dc036a1d02d0bcfb504bb76c6c2e8ee0ad4de5
LDHugeman <luisdavid.vp@gmail.com>
2021-11-25 10:16:27 +0100

    Clase 18

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
             System.out.println(availableAppointment.getDate() + " " +availableAppointment.getTime());
         }
 
-        /*System.out.println();
+        System.out.println();
         System.out.println();
 
         Patient patient = new Patient("Alejandra","alejandra@mail.com");
@@ -28,6 +28,23 @@ public class Main {
 
         patient2.setName("Manuel");
         System.out.println(patient.getName());
-        System.out.println(patient2.getName());*/
+        System.out.println(patient2.getName());
     }
+
+    /*public enum Day {
+        MONDAY("Lunes"),
+        TUESDAY("Jueves"),
+        FRIDAY("Viernes"),
+        SATURDAY("Sábado"),
+        SUNDAY("Domingo");
+
+        private String spanish;
+        private Day(String s) {
+            spanish = s;
+        }
+
+        private String getSpanish() {
+            return spanish;
+        }
+    }*/
 }
