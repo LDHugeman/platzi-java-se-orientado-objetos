b4ec31657c5560841f246fc8aad230070f4b8a46
LDHugeman <luisdavid.vp@gmail.com>
2021-11-25 09:08:49 +0100

    Clase 15

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,25 @@ public class Main {
         System.out.println(myDoctor.name);
         System.out.println(myDoctor.speciality);
 
+        System.out.println();
+        System.out.println();
+
         Patient patient = new Patient("Alejandra","alejandra@mail.com");
-        patient.setWeight(54.6);
+        Patient patient2 = new Patient("Anahí","anahi@mail.com");
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        patient2 = patient;
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        patient2.setName("Manuel");
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+       /* patient.setWeight(54.6);
         System.out.println(patient.getWeight());
 
         patient.setPhoneNumber("12345678");
-        System.out.println(patient.getPhoneNumber());
+        System.out.println(patient.getPhoneNumber());*/
     }
 }
